7873f38357bd4b620805338c14c986d33b60d5e9
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2019-02-10 16:12:06 +0400

    Added some more tests to check the error translation logic

--- __tests__/test_httperror.js
+++ __tests__/test_httperror.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const request = require('supertest')
 const utils = require('../index')
+const translationObject = require('./translations')
 
 describe('Express Utils', () => {
 
@@ -13,11 +14,33 @@ describe('Express Utils', () => {
             throw err
         })
 
+        app.get('/400/translated', (req, res) => {
+            req.locale = {
+                lang: 'ar'
+            }
+            const err = new Error()
+            err.statusCode = 400
+            err.message = 'invalidCredentials'
+            throw err
+        })
+
+        app.get('/400/untranslated', (req, res) => {
+            req.locale = {
+                lang: 'ar'
+            }
+            const err = new Error()
+            err.statusCode = 400
+            err.message = 'untranslated'
+            throw err
+        })
+
         app.get('/', (req, res) => {
             const err = new Error()
             throw err
         })
-        utils.errorHandler(app)
+        utils.errorHandler(app, null, {
+            translationObject
+        })
 
         test('should return with correct statusCode and body.message when middleware is applied', async () => {
             const resp = await request(app).get('/400')
@@ -29,6 +52,17 @@ describe('Express Utils', () => {
             const resp = await request(app).get('/')
             expect(resp.body.message).toEqual('Internal Server Error')
             expect(resp.statusCode).toEqual(500)
+        })
+
+        test('should return translated userMessage', async () => {
+            const resp = await request(app).get('/400/translated')
+            expect(resp.body.userMessage).toEqual('كلمة السر / البريد الالكتروني الذي تم إدخاله غير صحيح')
+            expect(resp.statusCode).toEqual(400)
+        })
+
+        test('should return raw untranslated usermessage if translations entry is not found', async () => {
+            const resp = await request(app).get('/400/untranslated')
+            expect(resp.body.userMessage).toEqual('untranslated')
         })
     })
 })
