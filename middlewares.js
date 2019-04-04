e7312816c82047dad5a9ae1b9cd9e39edcc4ab4a
Carles <carles.sanchez@namshi.com>
2019-04-04 14:13:37 +0400

    Added context-type middleware

--- middlewares.js
+++ middlewares.js
@@ -1,27 +1,46 @@
-const R = require('ramda');
-const {jsonOr} = require('./conversions');
-const { vsprintf } = require('sprintf-js')
-const _ = require('lodash')
+const R = require("ramda");
+const { jsonOr } = require("./conversions");
+const { vsprintf } = require("sprintf-js");
+const _ = require("lodash");
 
 const withDataOr = R.curry((key, defaultValue, req, res, next) => {
-    req[key] = jsonOr(req.get(key), defaultValue);
-    next();
+  req[key] = jsonOr(req.get(key), defaultValue);
+  next();
 });
 
-const withTranslate = R.curry(({
-    translations,
-    defaultLang,
-    localeHeaderKey,
-    localeQueryKey }, req, res, next) => {
+const hasContentTypes = (
+  contentTypes,
+  { status = 415, msg = { type: "error", msg: `Unsuported media type` } } = {}
+) => ({ headers }, res, next) =>
+  (!contentTypes.includes(headers["content-type"]) &&
+    res.status(status).send(msg)) ||
+  next();
 
-    const localeSplit = ((req.query && _.get(req.query, localeQueryKey, null)) || (req.headers && req.headers[localeHeaderKey]) || '').toLowerCase().split(/[-_]/g);
-    req.lang = localeSplit && localeSplit[0] || defaultLang;
-    const {lang} = req;
+const withTranslate = R.curry(
+  (
+    { translations, defaultLang, localeHeaderKey, localeQueryKey },
+    req,
+    res,
+    next
+  ) => {
+    const localeSplit = (
+      (req.query && _.get(req.query, localeQueryKey, null)) ||
+      (req.headers && req.headers[localeHeaderKey]) ||
+      ""
+    )
+      .toLowerCase()
+      .split(/[-_]/g);
+    req.lang = (localeSplit && localeSplit[0]) || defaultLang;
+    const { lang } = req;
     req.translate = (text, args = []) => {
-        const translation = translations[text] && translations[text][lang]? translations[text][lang] : text
-        return args.length ? vsprintf(translation, args) : translation;
-    }
-    next()
-});
+      const translation =
+        translations[text] && translations[text][lang]
+          ? translations[text][lang]
+          : text;
+      return args.length ? vsprintf(translation, args) : translation;
+    };
+    next();
+  }
+);
 
-module.exports = {withDataOr, withTranslate};
+module.exports = { withDataOr, withTranslate, hasContentTypes };
